eaf9b53448f2eeafdb63ea693539d43a768be774
M Rifqi Al Furqon <rifqi.alfurqon@doku.com>
2026-02-09 22:28:05 +0700

    add gpg

--- modules/common/gpg.nix
+++ modules/common/gpg.nix
@@ -1,17 +1,8 @@
 {pkgs, ...}: {
-  services = {
-    ## Enable gpg-agent with ssh support
-    gpg-agent = {
+  programs = {
+    gnupg.agent = {
       enable = true;
-      enableSshSupport = true;
-      enableZshIntegration = true;
-      # pinentry is a collection of simple PIN or passphrase dialogs used for
-      # password entry
-      pinentryPackage = pkgs.pinentry-tty;
     };
-
-    ## We will put our keygrip here
-    gpg-agent.sshKeys = [];
   };
 
   environment.systemPackages = with pkgs; [
